b31c0f9c4f09cbd0a19328a27c912601888f8b14
RomanYa83 <163516476+RomanYa83@users.noreply.github.com>
2024-03-25 14:18:46 +0300

    Add files via upload

---
+++ sheets_and_tuples.py
@@ -0,0 +1,8 @@
+immutable_var = 1, True, "list", 5.5
+print(immutable_var)
+# immutable_var[1] = False
+# print(immutable_var)
+mutable_list = [1, True, "list", 5.6]
+print(mutable_list)
+mutable_list[1] = False
+print(mutable_list)
