00c8632dced9ad711da27b426766056d77d20db0
RomanYa83 <163516476+RomanYa83@users.noreply.github.com>
2024-03-25 13:33:42 +0300

    Add files via upload

---
+++ Program_Organization.py
@@ -0,0 +1,8 @@
+my_string = input('Введите текст: ')
+print('Длина вашего текста составляет', len(my_string), 'символов')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print('В вашем тексте было', len(my_string) - len(my_string.replace(' ','')), 'пробела(-ов)')
+print(my_string[0])
+print(my_string[-1])
