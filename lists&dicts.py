6848825c7bcf422efdfdb0c230a21e7db116891e
RomanYa83 <163516476+RomanYa83@users.noreply.github.com>
2024-03-26 10:30:33 +0300

    Add files via upload

---
+++ lists&dicts.py
@@ -0,0 +1,14 @@
+my_list = ["apple", "coconut", "banana", "kiwi", "orange", "pineapple", "melon"]
+print('Your List: ', my_list)
+print('First element:', my_list[0])
+print('Last element:', my_list[-1])
+print(my_list[2:5])
+my_list[2] = "lemon"
+print(my_list)
+
+
+my_dict = {"apple":"яблоко", "coconut":"кокос", "banana":"банан", "kiwi":"киви", "orange":"апельсин", "pineapple":"ананас", "melon":"дыня"}
+print('Your Dictionary:', my_dict)
+print('Translate:', my_dict["melon"])
+my_dict.update({"watermelon":"арбуз","Alex": 79048856412})
+print('Mod_Dictionary:', my_dict)
