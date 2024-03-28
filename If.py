57d3783084eb9a334895be9d4a482f495a022b16
RomanYa83 <163516476+RomanYa83@users.noreply.github.com>
2024-03-28 14:00:56 +0300

    Add files via upload

---
+++ If.py
@@ -0,0 +1,35 @@
+IQ = 41
+
+print('Your IQ is', IQ)
+if IQ < 100:
+    print('Too few!')
+print('Good bay!')
+
+a, b = 4, 11
+
+if a > b:
+    print('a > b')
+if b > a:
+    print('Congratulation, b > a')
+if b > a and b != 0 or b < 500:
+    print('Luck!')
+
+if 4 < a and a < b:
+    print('OK')
+
+
+if '41' > '111':
+    print("Luck!")
+if '124' > '12':
+    print('Luck!')
+if [1, 3, 5, 7] > [1, 3, 5, 6, 8]:
+    print('Super!')
+
+# if '6' > 5:
+#     print('6 > 5')
+
+# if [4, 8] < 2:
+#     print('OK')
+
+if '6' != 6:
+    print('Right')
