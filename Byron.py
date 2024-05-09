a8c8ba19f7cf783c87fff99079bb1da846d32a9c
RomanYa83 <163516476+RomanYa83@users.noreply.github.com>
2024-05-09 21:41:37 +0300

    Add files via upload

---
+++ Byron.py
@@ -0,0 +1,25 @@
+file = open('Byron.txt', 'w')
+file.writelines(['My soul is dark - Oh! quickly string\n'
+                 'The harp I yet can brook to hear;\n'
+                 'And let thy gentle fingers fling\n'
+                 "Its melting murmurs o'er mine ear."
+                 'If in this heart a hope be dear,\n'
+                 'That sound shall charm it forth again:\n'
+                 'If in these eyes there lurk a tear,\n'
+                 "'Twill flow, and cease to burn my brain.\n"
+                 'But bid the strain be wild and deep,\n'
+                 'Nor let thy notes of joy be first:\n'
+                 'I tell thee, minstrel, I must weep,\n'
+                 'Or else this heavy heart will burst;\n'
+                 'For it hath been by sorrow nursed,\n'
+                 'And ached in sleepless silence, long;\n'
+                 "And now 'tis doomed to know the worst,\n"
+                 'And break at once - or yield to song.'])
+
+file.close()
+
+file = open('Byron.txt', 'r')
+b = file.read()
+print(b)
+
+file.close()
